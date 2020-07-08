ac797084738db36dad92eec5bb5069572161da5f
yogesh-hackx <yogesh.hackx@gmail.com>
2020-07-08 23:09:19 +0530

    :tada: Add Head Layout

--- Components/Layout/Layout.js
+++ Components/Layout/Layout.js
@@ -1,7 +1,16 @@
+import Head from 'next/head';
 import Navbar from './Navbar';
 
 const Layout = (props) => (
     <>
+        <Head>
+            <meta charset="UTF-8" />
+            <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1.0"
+            />
+            <title>{props.title || 'Grocery Store'}</title>
+        </Head>
         <Navbar />
         <div className="container">{props.children}</div>
     </>
