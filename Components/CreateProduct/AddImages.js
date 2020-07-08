33e6383bc26755b1a163ee0e66dd5c497ea288ae
yogesh-hackx <yogesh.hackx@gmail.com>
2020-07-08 23:11:45 +0530

    :tada: Add Add Image Form

---
+++ Components/CreateProduct/AddImages.js
@@ -0,0 +1,11 @@
+export default function AddImages(props) {
+    // TODO: Add image upload and generate URL's for Form state
+    return (
+        <div className="imageUploadForm">
+            {/* TODO: Work on Image Upload UI */}
+            <input type="file" name="mainImg" />
+            <input type="file" name="image2" />
+            <input type="file" name="image3" />
+        </div>
+    );
+}
