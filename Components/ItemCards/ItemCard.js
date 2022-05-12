209251195a20ec97fb057fe454e9791d4eef3e53
HaritSharma-prog <haritsharma058@gmail.com>
2022-05-12 10:51:50 +0530

    add: dummy data

--- Components/ItemCards/ItemCard.js
+++ Components/ItemCards/ItemCard.js
@@ -1,20 +1,20 @@
 import styles from './ItemCard.module.sass';
 
-const ItemCard = (props) => {
+const ItemCard = ({name, mrp, sellingPrice, imgURL}) => {
     return (
         <div className={styles.itemCard}>
             <div className={styles.imgContainer}>
-                <img src="assets/tomato.png" alt="" className={styles.itemImage} />
+                <img src={imgURL} alt="" className={styles.itemImage} />
             </div>
             <div className={styles.priceContainer}>
                 <span className={styles.currentPrice}>
-                    <span className={styles.rupeesSymbol}>&#8377;</span> 165
+                    <span className={styles.rupeesSymbol}>&#8377;</span> {sellingPrice}
                 </span>
                 <span className={styles.oldPrice}>
-                    <span className={styles.rupeesSymbol}>&#8377;</span>180
+                    <span className={styles.rupeesSymbol}>&#8377;</span>{mrp}
                 </span>
             </div>
-            <div className={styles.itemTitle}>Fresh Tomato - 1 Kg</div>
+            <div className={styles.itemTitle}>{name}</div>
             <div className={styles.addButtonContainer}>ADD</div>
         </div>
     );
